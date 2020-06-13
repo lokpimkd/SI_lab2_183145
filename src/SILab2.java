dfc4efd4a4f6216684c7f026d744d5e415910b15
lokpimkd <63260854+lokpimkd@users.noreply.github.com>
2020-06-13 16:55:52 +0200

    Add files via upload

--- src/SILab2.java
+++ src/SILab2.java
@@ -27,33 +27,33 @@ class User {
 public class SILab2 {
 
     public boolean function (User user, List<String> allUsers) {
-        String specialCharacters="!#$%&'()*+,-./:;<=>?@[]^_`{|}";
-        if (user==null) {
-            throw new RuntimeException("The user is not instantiated");
+        String specialCharacters="!#$%&'()*+,-./:;<=>?@[]^_`{|}"; //A
+        if (user==null) {//B
+            throw new RuntimeException("The user is not instantiated");//C
         }
-        if (user.getUsername()==null || user.getPassword()==null)
-            throw new RuntimeException("The user is missing some mandatory information");
-        String password = user.getPassword();
-        String passwordLower = password.toLowerCase();
-        if (passwordLower.contains(user.getUsername().toLowerCase())) {
-            return false;
+        if (user.getUsername()==null || user.getPassword()==null)//D
+            throw new RuntimeException("The user is missing some mandatory information");//E
+        String password = user.getPassword();//F
+        String passwordLower = password.toLowerCase();//G	
+        if (passwordLower.contains(user.getUsername().toLowerCase())) { //H
+            return false;//J
         }
-        else if (passwordLower.length()<8)
-            return false;
-        else {
-            boolean digit = false, upper = false, special = false;
+        else if (passwordLower.length()<8)//K
+            return false;//L
+        else {//M
+            boolean digit = false, upper = false, special = false;//N
 
-            for (int i=0;i<password.length();i++) {
-                if (Character.isDigit(password.charAt(i)))
-                    digit = true;
-                if (Character.isUpperCase(password.charAt(i)))
-                    upper = true;
-                if (specialCharacters.contains(String.valueOf(password.charAt(i))))
-                    special = true;
+            for (int i=0;i<password.length();i++) {//O
+                if (Character.isDigit(password.charAt(i)))//P
+                    digit = true;//R
+                if (Character.isUpperCase(password.charAt(i)))//S
+                    upper = true;//T
+                if (specialCharacters.contains(String.valueOf(password.charAt(i))))//W
+                    special = true;//W
             }
-            if (!digit || !upper || !special)
-                return false;
+            if (!digit || !upper || !special)//X
+                return false;//Y
         }
-        return true;
-    }
+        return true;//Z
+    } //END
 }
